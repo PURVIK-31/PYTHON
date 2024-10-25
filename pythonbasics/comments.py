7068ac9eec52a70936a8e2f63e0cfff65d93a7ca
PURVIK-31 <mepurviksharma@gmail.com>
2024-10-25 13:33:36 +0530

    learnt comments

---
+++ pythonbasics/comments.py
@@ -0,0 +1,9 @@
+print("LETS SEE SOME COMMENTS : ")
+
+# this is a comment sir 
+print("FIRST IS SINGLE LINE COMMENT ~ ")
+print("THIS IS A MULTI LINE COMMENT :  ")
+"""
+print("ignored op ~~")
+HI THERE GURURJI
+"""
